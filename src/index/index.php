61fc4a898ce1c4c13b4716f436b5c7e99815a46e
jeanziin <jjeanfreitass@gmail.com>
2022-12-31 01:02:05 -0300

    Fifth-commit

--- src/index/index.php
+++ src/index/index.php
@@ -37,7 +37,7 @@ if (isset($_POST["num"])) {
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <link rel="stylesheet" href="../style_index.css">
-  
+
 
 </head>
 
@@ -63,25 +63,25 @@ if (isset($_POST["num"])) {
               </script>
               <div class="alert alert-success">
                 <?php
-                if ((int)$num % 2 == 0) {
+                if ((int)$num % 2 == 0 && $num >= 4) {
                   echo  Desafio_Conjectura_Goldbach($num);
                 } else {
-                  echo "Esperando um Número Válido (Par maior ou igual a 4)...";
+                  echo "Insira um Número Par maior ou igual a 4!";
                 }
                 ?>
               </div>
-                   <div class="direita">
+              <div class="direita">
                 <img src="../style/math.png" alt="imagem">
                 <div class="sombra"></div>
-            </div>
-                </div>
               </div>
             </div>
-          </form>
         </div>
-        <div>
-        </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+      </div>
+      </form>
+    </div>
+    <div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
   </section>
 </body>
 
--- src/index/teste.php
+++ src/index/teste.php
@@ -5,39 +5,33 @@ function ChecarNumeroPrimo($num)
   if (is_numeric($num)) {
 
     if ($num == 1)
-      return 0;
-
+      return "Número Primo!";
+    
     for ($cont = 2; $cont <= (int)$num / 2; $cont++) {
       if ($num % $cont == 0)
-        return 0;
+        return "Não é um Número Primo!";
     }
-    return 1;
+    return "Número Primo!";
   } else {
     echo "O Valor digitado não é um Valor númerico.";
   }
 }
 
-$num = 6;
-$checar = ChecarNumeroPrimo($num);
-/* if ($checar == 1)
-    echo "Número Primo!";
-else
-    echo "Não é um Número Primo!";
-*/
 
 function  Desafio_Conjectura_Goldbach($num)
 {
   if (is_numeric($num)) {
-    for ($cont = 4; $num >= $cont; $cont += 2) {
+    for ($cont = 4; $num >= $cont; $cont+=2) {
 
       for ($num_Primo = 2; $num_Primo < $cont; $num_Primo++) {
 
         if ($num_Primo < 2 || $num_Primo % 2 != 0) {
 
           if ($num_Primo + ($cont - $num_Primo) == $cont) {
+
+            $VerificarSoma = $cont - $num_Primo;
+            echo "Conjectura Goldbach verdadeira para: $cont <br> sendo $cont = $num_Primo + $VerificarSoma" . "\n";
           }
-          $VerificarSoma = $cont - $num_Primo;
-          echo "Conjectura Goldbach verdadeira para: $cont <br> sendo $cont = $num_Primo + $VerificarSoma" . "\n";
         }
       }
     }
@@ -45,3 +39,5 @@ function  Desafio_Conjectura_Goldbach($num)
     echo "O Valor digitado não é um Valor númerico.";
   }
 }
+
+$num = 0;
